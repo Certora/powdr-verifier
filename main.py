b000cf143bd7df8facd54cdbb0c8dd8ca7f43a19
Gereon Kremer <gereon.kremer@gmail.com>
2025-12-19 08:24:07 +0100

    Some initial work

---
+++ main.py
@@ -0,0 +1,26 @@
+import logging
+import pprint
+
+from src.checks import *
+from src.utils import *
+from src.smt import *
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+    parse_args()
+    before = load_json(args().input_before, 'Before')
+    after = load_json(args().input_after, 'After')
+
+    before_i2n, before_n2i = collect_variables(before)
+    after_i2n, after_n2i = collect_variables(after)
+
+    #print(sorted(before_i2n.items()))
+    #print(sorted(after_i2n.items()))
+
+    before_smt = load_smt_formula(before)
+    after_smt = load_smt_formula(after)
+
+    if is_equivalent(before_smt, after_smt):
+        print("The two programs are equivalent")
+    else:
+        print("The two programs are not equivalent")
