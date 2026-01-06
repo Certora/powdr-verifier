578e7ca17746141f9f01c8c44ca43897cee382ec
Gereon Kremer <gereon.kremer@gmail.com>
2026-01-06 12:58:56 +0100

    add utility wrap_mod

---
+++ src/smt_utils.py
@@ -0,0 +1,12 @@
+from pysmt.fnode import FNode
+from pysmt.shortcuts import *
+from typing import Optional
+
+from .utils import ARGS
+
+UF_MOD = Symbol('uf_mod', FunctionType(INT, [INT, INT]))
+
+def wrap_mod(input: FNode, modulus: Optional[FNode] = None) -> FNode:
+    if modulus is None:
+        modulus = Int(ARGS().field_type.value)
+    return Function(UF_MOD, [input, modulus])
