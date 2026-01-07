4abf52de7ff3f32052cd6381df56cd4b89862acb
Gereon Kremer <gereon.kremer@gmail.com>
2026-01-07 11:23:20 +0100

    fix encoding

--- src/encoding.py
+++ src/encoding.py
@@ -14,13 +14,12 @@ def build_vc(f1: FormulaWithAxioms, f2: FormulaWithAxioms) -> FNode:
     var1 = collect_variables(f1)
     var2 = collect_variables(f2)
 
-    shared = var1 & var2
     onlyfirst = var1 - var2
 
-    f = And(
-        ForAll(onlyfirst, 
+    f = ForAll(onlyfirst,
+        And(
             Not(Iff(f1.formula, f2.formula)),
-        ),
-        *frozenset(f1.axioms + f2.axioms + f1.derived + f2.derived),
+            *frozenset(f1.axioms + f2.axioms + f1.derived + f2.derived),
+        )
     )
     return rewrite(f)
