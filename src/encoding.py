2986c9aef618f4a483452222384d2a3198c62b1b
Gereon Kremer <gereon.kremer@gmail.com>
2026-01-07 11:05:24 +0100

    add encoding file

---
+++ src/encoding.py
@@ -0,0 +1,26 @@
+from .smt import *
+
+def collect_variables(data: FormulaWithAxioms) -> frozenset[FNode]:
+    return (
+        data.formula.get_free_variables() |
+        frozenset.union(
+            *[ axiom.get_free_variables() for axiom in data.axioms ],
+            *[ derived.get_free_variables() for derived in data.derived ]
+        )
+    )
+
+def build_vc(f1: FormulaWithAxioms, f2: FormulaWithAxioms) -> FNode:
+
+    var1 = collect_variables(f1)
+    var2 = collect_variables(f2)
+
+    shared = var1 & var2
+    onlyfirst = var1 - var2
+
+    f = And(
+        ForAll(onlyfirst, 
+            Not(Iff(f1.formula, f2.formula)),
+        ),
+        *frozenset(f1.axioms + f2.axioms + f1.derived + f2.derived),
+    )
+    return rewrite(f)
