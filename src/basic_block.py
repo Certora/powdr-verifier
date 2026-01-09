73f997a70fcc19ae566f1ab9308503b5d5a67145
Gereon Kremer <gereon@certora.com>
2026-01-09 10:42:49 +0100

    add basic block

---
+++ src/basic_block.py
@@ -0,0 +1,9 @@
+class BasicBlock:
+    def __init__(self, data: dict):
+        assert "start_pc" in data, "basic block has no start_pc"
+        assert "statements" in data, "basic block has no statements"
+        self.start_pc = data["start_pc"]
+        self.statements = data["statements"]
+
+    def __eq__(self, other: 'BasicBlock') -> bool:
+        return self.start_pc == other.start_pc and self.statements == other.statements
