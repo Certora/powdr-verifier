77430ab84dff84e9630a91b803964b96d99d0d78
Gereon Kremer <gereon.kremer@gmail.com>
2026-01-09 12:45:48 +0100

    add sequencing for execution bridge

--- src/bus_interactions/openvm_execution_bridge.py
+++ src/bus_interactions/openvm_execution_bridge.py
@@ -5,7 +5,16 @@ from pysmt.typing import *
 from pysmt.fnode import FNode
 
 from .single_interaction_encoder import SingleInteractionEncoder
+from .sequencing import encode_sequencing
 
 class OpenVMExecutionBridgeEncoder(SingleInteractionEncoder):
-    def encode(self) -> FNode:
+    def __init__(self, name: str) -> None:
+        super().__init__(name)
+        self.interactions = []
+
+    def encode(self, mult: FNode, pc: FNode, timestamp: FNode) -> FNode:
+        self.interactions.append((pc, timestamp))
         return TRUE()
+
+    def get_axioms(self) -> list[FNode]:
+        return encode_sequencing(f'{self.name}_eb', self.interactions)
---
+++ src/bus_interactions/sequencing.py
@@ -0,0 +1,32 @@
+from pysmt.fnode import FNode
+from pysmt.shortcuts import *
+from pysmt.typing import *
+
+# encodes a formula that establishes a sequencing of the given interactions
+# each interaction is a tuple of (data, timestamp) and a sequencing imposes a
+# total order on the zero-indexed interactions.
+# an interaction with even index is followed by an interaction such that the
+# data is the same and the timestamp is greater.
+# an interaction with odd index is followed by an interaction such that the
+# data is different and the timestamp is the same.
+def encode_sequencing(id: str, interactions: list[tuple[FNode, FNode]]) -> FNode:
+
+    n = len(interactions)
+
+    datas = [ Symbol(f'seqd_{id}_{i}', INT) for i in range(n) ]
+    timestamps = [ Symbol(f'seqt_{id}_{i}', INT) for i in range(n) ]
+
+    return And(
+        *[Or(
+            Equals(datas[i], interactions[j][0]) for j in range(n)
+        ) for i in range(n)],
+        *[Or(
+            Equals(timestamps[i], interactions[j][1]) for j in range(n)
+        ) for i in range(n)],
+        *[
+                And(Equals(datas[i], datas[i+1]), GT(timestamps[i+1], timestamps[i]))
+            if i % 2 == 0 else
+                And(NotEquals(datas[i], datas[i+1]), Equals(timestamps[i], timestamps[i+1]))
+            for i in range(n-1)
+        ],
+    )
