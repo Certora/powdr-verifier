feeb4d4d44f1fe2fe6733fcbafbcd745143414a8
Gereon Kremer <gereon@certora.com>
2026-01-09 12:45:29 +0100

    add name to distinguish variables for before and after formula

--- src/bus_interactions/single_interaction_encoder.py
+++ src/bus_interactions/single_interaction_encoder.py
@@ -2,5 +2,8 @@ from pysmt.fnode import FNode
 from pysmt.shortcuts import *
 
 class SingleInteractionEncoder:
+    def __init__(self, name: str):
+        self.name = name
+
     def get_axioms(self) -> list[FNode]:
         return TRUE()
