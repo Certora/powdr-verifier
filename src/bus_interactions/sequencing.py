e6f79e14d232c77f4134b99c862c54b1a7e5046e
Gereon Kremer <gereon.kremer@gmail.com>
2026-01-12 09:09:29 +0100

    work on sequencing encoding

--- src/bus_interactions/sequencing.py
+++ src/bus_interactions/sequencing.py
@@ -4,26 +4,31 @@ from ..smt_utils import *
 # each interaction is a tuple of (data, timestamp) and a sequencing imposes a
 # total order on the zero-indexed interactions.
 # an interaction with even index is followed by an interaction such that the
-# data is the same and the timestamp is greater.
+# timestamp is greater.
 # an interaction with odd index is followed by an interaction such that the
-# data is different and the timestamp is the same.
+# data and the timestamp are the same.
 def encode_sequencing(id: str, interactions: list[tuple[FNode, FNode]]) -> FNode:
 
     n = len(interactions)
 
-    datas = [ Symbol(f'seqd_{id}_{i}', INT) for i in range(n) ]
-    timestamps = [ Symbol(f'seqt_{id}_{i}', INT) for i in range(n) ]
+    datas = [ Symbol(f'seq_{id}_{i}d', INT) for i in range(n) ]
+    timestamps = [ Symbol(f'seq_{id}_{i}t', INT) for i in range(n) ]
 
     return And(
         # each data is equal to one of the interactions
-        *[ Or(Equals(datas[i], interactions[j][0]) for j in range(n)) for i in range(n)],
-        # each timestamp is equal to one of the interactions
-        *[ Or(Equals(timestamps[i], interactions[j][1]) for j in range(n)) for i in range(n)],
+        *[
+            Or(
+                And(
+                    Equals(datas[i], interactions[j][0]),
+                    Equals(timestamps[i], interactions[j][1])
+                ) for j in range(n)
+            ) for i in range(n)
+        ],
         # sequencing on datas and timestamps
         *[
-                And(Equals(datas[i], datas[i+1]), GT(timestamps[i+1], timestamps[i]))
+                GT(timestamps[i+1], timestamps[i])
             if i % 2 == 0 else
-                And(NotEquals(datas[i], datas[i+1]), Equals(timestamps[i], timestamps[i+1]))
+                And(Equals(datas[i], datas[i+1]), Equals(timestamps[i], timestamps[i+1]))
             for i in range(n-1)
         ],
     )
