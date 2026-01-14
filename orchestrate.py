1c6e674ecda7703320a0a70bd276287fbff49674
Gereon Kremer <gereon@certora.com>
2026-01-14 10:15:44 +0100

    work on orchestrate script

--- orchestrate.py
+++ orchestrate.py
@@ -1,3 +1,5 @@
+import argparse
+import logging
 import subprocess
 from pathlib import Path
 
@@ -9,13 +11,21 @@ assert DATA_DIR.exists()
 assert POWDR_DIR.exists()
 assert VERIFIER_DIR.exists()
 
-def run_powdr():
-    POWDR_CONFIGS = [
-        f"APC_CBOR_PATH={DATA_DIR.relative_to(POWDR_DIR / "openvm", walk_up=True)} cargo test single_add_1 -- --no-capture"
-    ]
-    for config in POWDR_CONFIGS:
-        print(f"Running {config}")
-        subprocess.run(config, shell=True, cwd=POWDR_DIR)
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('command', type=str, choices=['trace', 'eval', 'verify'])
+    parser.add_argument('tests', type=str, nargs='+', default=['single_add_1'])
+
+    return parser.parse_args()
+
+
+def run_powdr(tests):
+    for test in tests:
+        cmd = [
+            f"APC_CBOR_PATH={DATA_DIR.relative_to(POWDR_DIR / "openvm", walk_up=True)} cargo test {test} -- --no-capture"
+        ]
+        logging.info(f"running {cmd}")
+        subprocess.run(cmd, shell=True, cwd=POWDR_DIR)
 
 def deserialize(cbor_file):
     DESERIALIZE_CONFIGS = [
@@ -32,11 +42,50 @@ def deserialize_all():
         print(f"Deserializing {cbor_file}")
         deserialize(cbor_file)
 
+def run_tracer():
+    pass
+
+def run_evaluator():
+    pass
+
 def run_verifier():
     subprocess.run([
         "python3", VERIFIER_DIR / "main.py", "--dump-smt", "verify", DATA_DIR / "apc_candidate_unopt_0.json", DATA_DIR / "apc_candidate_0.json"
     ])
 
-run_powdr()
-deserialize_all()
-run_verifier()
+
+if __name__ == '__main__':
+    args = parse_args()
+
+    run_powdr(args.tests)
+
+    match args.command:
+        case 'trace':
+            logging.info(f"running tracer on {args.input}")
+            input = load_json(args.input, 'input')
+
+            smt = convert_to_smt_formula("input", input, BasicBlock(input["block"]))
+
+            trace(smt)
+
+        case 'eval':
+            logging.info(f"evaluating trace from {args.model} on {args.input}")
+            input = load_json(args.input, 'input')
+            model = load_json(args.model, 'model')
+
+            smt = convert_to_smt_formula("input", input, BasicBlock(input["block"]))
+
+            evaluate(input["machine"], smt, model)
+
+        case 'verify':
+            logging.info(f"verify equivalence of {args.input_before} and {args.input_after}")
+            before = load_json(args.input_before, 'Before')
+            after = load_json(args.input_after, 'After')
+
+            before_block = BasicBlock(before["block"])
+            assert before_block == BasicBlock(after["block"]), "The basic block has changed"
+
+            verify(before, after, before_block)
+        case _:
+            logging.error(f"unknown command: {args.command}")
+            exit(1)
