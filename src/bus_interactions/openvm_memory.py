1ebdc7fba3d8c050c8fbe8c03372dc7fc7a5173e
Gereon Kremer <gereon.kremer@gmail.com>
2026-01-14 15:34:14 +0100

    nicer naming of vars

--- src/bus_interactions/openvm_memory.py
+++ src/bus_interactions/openvm_memory.py
@@ -14,6 +14,7 @@ class OpenVMMemoryEncoder(SingleInteractionEncoder):
     def __init__(self, name: str) -> None:
         super().__init__(name)
         self.interactions = {}
+        self.name_or_id = NameOrIdGenerator()
     
     def __add_interaction(self, mult: FNode, address_space: FNode, pointer: FNode, data: list[FNode], timestamp: FNode) -> None:
         if (address_space, pointer) not in self.interactions:
@@ -32,7 +33,6 @@ class OpenVMMemoryEncoder(SingleInteractionEncoder):
     def get_axioms(self) -> list[FNode]:
         encode_timestamps = lambda i1, i2: LT(i1[4], i2[4])
         return And(
-            encode_permutation_check(f'{self.name}_mem_{address_space}_{pointer}', interactions, encode_timestamps)
+            encode_permutation_check(f'{self.name}_mem_{self.name_or_id(address_space)}_{self.name_or_id(pointer)}', interactions, encode_timestamps)
             for (address_space, pointer), interactions in self.interactions.items()
         )
-
