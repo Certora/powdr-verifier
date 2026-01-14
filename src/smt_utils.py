6a0d5d84bb50fdbd7a71beaf7d3b9e6d2c641da1
Gereon Kremer <gereon@certora.com>
2026-01-14 15:34:31 +0100

    allow using cvc5 with ff solver

--- src/smt_utils.py
+++ src/smt_utils.py
@@ -11,6 +11,9 @@ UF_MOD = Symbol('uf_mod', FunctionType(INT, [INT, INT]))
 REAL_MOD = Symbol('mod', FunctionType(INT, [INT, INT]))
 
 logics.PYSMT_LOGICS = logics.PYSMT_LOGICS | frozenset([logics.QF_UFNIA, logics.UFNIA])
+get_env().factory.add_generic_solver('cvc5ff', [
+    'cvc5/build/bin/cvc5', '--mod-range-solver', '--nia-intro-mm-mod'
+], [logics.QF_UFNIA, logics.UFNIA])
 
 def wrap_mod(input: FNode, modulus: Optional[FNode] = None) -> FNode:
     if modulus is None:
@@ -26,3 +29,12 @@ def to_nice_model(model: Any) -> dict[str, Any]:
     return {
         str(k): as_constant(v) for k,v in sorted(model, key=lambda x: str(x))
     }
+
+class NameOrIdGenerator:
+    def __init__(self):
+        self.mapping = {}
+    
+    def __call__(self, x: FNode) -> str:
+        if x.is_constant() or x.is_symbol():
+            return str(x)
+        return self.mapping.setdefault(x, len(self.mapping))
