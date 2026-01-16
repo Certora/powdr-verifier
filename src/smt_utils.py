30b3625ed72b43c3ccb831287af1539dfd37b4a6
Gereon Kremer <gereon.kremer@gmail.com>
2026-01-16 22:10:01 +0100

    monkey-patch pysmt

--- src/smt_utils.py
+++ src/smt_utils.py
@@ -4,14 +4,6 @@ from .utils import ARGS
 
 from .smt_backends.pysmt import *
 
-UF_MOD = Symbol('uf_mod', FunctionType(INT, [INT, INT]))
-REAL_MOD = Symbol('mod', FunctionType(INT, [INT, INT]))
-
-def wrap_mod(input: FNode, modulus: Optional[FNode] = None) -> FNode:
-    if modulus is None:
-        modulus = Int(ARGS().field_type.value)
-    return Function(UF_MOD, [input, modulus])
-
 def with_comment(f: FNode, comment: str) -> FNode:
     setattr(f, 'comment', comment)
     return f
@@ -41,25 +33,3 @@ class NameOrIdGenerator:
         if x.is_constant() or x.is_symbol():
             return str(x)
         return self.mapping.setdefault(x, len(self.mapping))
-
-def convert_to_smt_script(f: FNode, logic: logics.Logic) -> script.SmtLibScript:
-    smtlib = script.smtlibscript_from_formula(f, logic)
-
-    # replace "declare-fun uf_mod" by "define-fun uf_mod"
-    for id,cmd in enumerate(smtlib.commands):
-        match cmd:
-            case script.SmtLibCommand(name='declare-fun') if cmd.args == [UF_MOD]:
-                args = [Symbol('x', INT), Symbol('y', INT)]
-                define_fun = script.SmtLibCommand(
-                    name='define-fun',
-                    args=[UF_MOD, args, INT, Function(REAL_MOD, args)]
-                )
-                smtlib.commands[id] = define_fun
-            case _:
-                pass
-
-    # add model production and model retrieval
-    smtlib.commands.insert(1, script.SmtLibCommand(name='set-option', args=[':produce-models', 'true']))
-    smtlib.commands.insert(2, script.SmtLibCommand(name='set-option', args=[':incremental', 'true']))
-    smtlib.add_command(script.SmtLibCommand(name='get-model', args=[]))
-    return smtlib
