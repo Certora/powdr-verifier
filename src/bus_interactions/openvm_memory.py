1f177cff2e82c6fbd443a315e7789b41f88eefba
Gereon Kremer <gereon.kremer@gmail.com>
2026-01-16 22:11:33 +0100

    started writing a memory analysis

--- src/bus_interactions/openvm_memory.py
+++ src/bus_interactions/openvm_memory.py
@@ -6,6 +6,49 @@ from .single_interaction_encoder import SingleInteractionEncoder
 
 from ..smt_utils import *
 
+class MemoryAnalysis:
+    def __init__(self, constraints: list[FNode]):
+        self.representatives = []
+        self.belongs_to = {}
+        self.solver = SolverFor(logic=QF_UFNIA)
+        for c in constraints:
+            self.solver.add(c)
+    
+    def add_memory_access(self, address_space: FNode, pointer: FNode):
+        if (address_space, pointer) in self.belongs_to:
+            return
+        implied = []
+        possibly = []
+
+        for rid,(ras,rp) in enumerate(self.representatives):
+            same = And(Equals(ras, address_space), Equals(rp, pointer))
+            if is_valid(self.solver, same):
+                # aliasing is implied
+                print(f"aliasing is implied for {same}")
+                implied.append(rid)
+                break
+            elif is_sat(self.solver, same):
+                # aliasing is possible
+                print(f"aliasing is possible for {same}")
+                possibly.append(rid)
+
+        if implied:
+            # a single implied alias is found
+            assert len(implied) == 1, "Multiple implied aliases for the same memory access"
+            assert len(possibly) == 0, "Implied alias and possible aliases for the same memory access"
+            self.belongs_to[(address_space, pointer)] = implied[0]
+        elif possibly:
+            # one or more possible aliases are found
+            rid = len(self.representatives)
+            self.representatives.append((address_space, pointer))
+            self.belongs_to[(address_space, pointer)] = [rid] + possibly
+        else:
+            # no aliases are found
+            rid = len(self.representatives)
+            self.representatives.append((address_space, pointer))
+            self.belongs_to[(address_space, pointer)] = rid
+
+
 class OpenVMMemoryEncoder(SingleInteractionEncoder):
     """
     Encodes memory bus interactions. It implements a permutation check on all
@@ -16,10 +59,19 @@ class OpenVMMemoryEncoder(SingleInteractionEncoder):
         self.interactions = {}
         self.name_or_id = NameOrIdGenerator()
     
-    def __add_interaction(self, mult: FNode, address_space: FNode, pointer: FNode, data: list[FNode], timestamp: FNode) -> None:
-        if (address_space, pointer) not in self.interactions:
-            self.interactions[(address_space, pointer)] = []
-        self.interactions[(address_space, pointer)].append((mult, data + [timestamp]))
+    def _sorted_interactions(self) -> list[tuple[FNode, Any]]:
+        return sorted(self._interactions, key=lambda i: size(i[1][0]) + size(i[1][1]))
+
+    def pre_analysis(self) -> None:
+        print(f"#constraints: {len(self.constraints())}")
+        #m = MemoryAnalysis(self.constraints())
+        #for mult, args in self._sorted_interactions():
+        #    address_space, pointer, *data, timestamp = args
+        #    m.add_memory_access(address_space, pointer)
+        #
+        #print(m.representatives)
+        #print(m.belongs_to)
+
 
     def encode(self, mult: FNode, address_space: FNode, pointer: FNode, data: list[FNode], timestamp: FNode) -> FNode:
         return with_comment(
