7664f92ff7a6fd72feaaa247e92d13dacdf9713c
Gereon Kremer <gereon@certora.com>
2026-01-21 10:26:03 +0100

    better file handling in orchestration

--- orchestrate.py
+++ orchestrate.py
@@ -14,23 +14,24 @@ assert VERIFIER_DIR.exists()
 
 def parse_args():
     parser = argparse.ArgumentParser()
+    parser.add_argument('--clean', action='store_true')
 
     sub = parser.add_subparsers(dest="command")
     
     sub_trace = sub.add_parser('trace')
-    sub_trace.add_argument('test', type=str, default='single_add_1')
+    sub_trace.add_argument('test', type=str)
 
     sub_traceall = sub.add_parser('trace-all')
-    sub_traceall.add_argument('test', type=str, default='single_add_1')
+    sub_traceall.add_argument('test', type=str)
 
     sub_eval = sub.add_parser('eval')
-    sub_eval.add_argument('test', type=str, default='single_add_1')
+    sub_eval.add_argument('test', type=str)
 
     sub_verify = sub.add_parser('verify-end2end')
-    sub_verify.add_argument('test', type=str, default='single_add_1')
+    sub_verify.add_argument('test', type=str)
 
     sub_verifyall = sub.add_parser('verify-stepwise')
-    sub_verifyall.add_argument('test', type=str, default='single_add_1')
+    sub_verifyall.add_argument('test', type=str)
 
     sub_ppsmt = sub.add_parser('preprocess')
     sub_ppsmt.add_argument('file', type=Path)
@@ -40,38 +41,43 @@ def parse_args():
 
 def run_powdr(test):
     cmd = [
-        f"APC_EXPORT_PATH={DATA_DIR.relative_to(POWDR_DIR / "openvm", walk_up=True)} APC_EXPORT_LEVEL=2 cargo test {test} -- --no-capture --exact"
+        f"APC_EXPORT_PATH={DATA_DIR.relative_to(POWDR_DIR / "openvm", walk_up=True)}",
+        "APC_EXPORT_LEVEL=2",
+        f"cargo test {test} -- --no-capture --exact",
     ]
     logging.warning(f"running {' '.join(cmd)}")
-    subprocess.run(cmd, shell=True, cwd=POWDR_DIR)
-
-def run_verifier():
-    subprocess.run([
-        "python3", VERIFIER_DIR / "main.py", "--dump-smt", "verify", DATA_DIR / "apc_candidate_unopt_0.json", DATA_DIR / "apc_candidate_0.json"
-    ])
-
+    subprocess.run(' '.join(cmd), shell=True, cwd=POWDR_DIR)
+    for file in DATA_DIR.glob("apc_candidate_*.*"):
+        new = file.with_name(file.name.replace("apc_candidate", test))
+        file.rename(new)
+        if new.suffix == ".json":
+            yield new
 
 if __name__ == '__main__':
     args = parse_args()
 
-    files = sorted(list(DATA_DIR.glob("apc_candidate_0_*.json")))
+    if args.clean:
+        for file in DATA_DIR.glob(f"{args.test}_*"):
+            file.unlink()
+
+    files = sorted(run_powdr(args.test))
     first = files[0]
     last = files[-1]
 
     match args.command:
         case 'trace':
-            run_powdr(args.test)
             subprocess.run([
                 "python3", VERIFIER_DIR / "main.py",
+                "-v",
+                "--skip-memory-analysis",
                 "--dump-smt",
                 "--base-dump", first,
                 "trace",
-                last,
+                first,
                 "--dump-model", last.with_suffix(".model"),
             ])
 
         case 'trace-all':
-            run_powdr(args.test)
             for f in files:
                 subprocess.run([
                     "python3", VERIFIER_DIR / "main.py",
@@ -83,7 +89,6 @@ if __name__ == '__main__':
                 ])
 
         case 'eval':
-            run_powdr(args.test)
             model = last.with_suffix(".model")
             subprocess.run([
                 "python3", VERIFIER_DIR / "main.py",
@@ -102,7 +107,6 @@ if __name__ == '__main__':
             ])
 
         case 'verify-end2end':
-            run_powdr(args.test)
             subprocess.run([
                 "python3", VERIFIER_DIR / "main.py",
                 "--dump-smt",
@@ -113,7 +117,6 @@ if __name__ == '__main__':
             ])
 
         case 'verify-stepwise':
-            run_powdr(args.test)
             for a,b in itertools.pairwise(files):
                 subprocess.run([
                     "python3", VERIFIER_DIR / "main.py",
@@ -123,14 +126,6 @@ if __name__ == '__main__':
                     a,
                     b,
                 ])
-        
-        case 'preprocess':
-            subprocess.run([
-                "cvc5/build/bin/cvc5",
-                "--preprocess-only",
-                "-o", "post-asserts",
-                args.file,
-            ])
 
         case _:
             logging.error(f"unknown command: {args.command}")
