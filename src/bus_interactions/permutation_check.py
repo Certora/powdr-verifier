d82a0a98dc7a39e42ecbd3930387dc1cca90582f
Gereon Kremer <gereon@certora.com>
2026-01-26 15:44:26 +0100

    pull timestamp checks out of permutation check

--- src/bus_interactions/permutation_check.py
+++ src/bus_interactions/permutation_check.py
@@ -1,23 +1,22 @@
-from itertools import pairwise
+from itertools import batched, pairwise
 from typing import Callable
 from ..smt.utils import *
 
+def ordered_timestamp_check(
+    interaction_timestamps: list[FNode],
+) -> FNode:
+    return And(
+        LT(i, j) for i,j in batched(interaction_timestamps, 2)
+    )
+
 def ordered_permutation_check(
-    interactions: list[tuple[tuple[FNode, list[FNode]], bool]],
-    additional: Callable[[list[FNode], list[FNode]], FNode] = lambda _: TRUE()
+    interactions: list[tuple[tuple[FNode, list[FNode]], bool]]
 ) -> FNode:
     """
-    Encodes a permutation check for the given list of interactions.
-    Every odd-indexed interaction permutes with the subsequent even-indexed
-    interaction: their data are equivalent and their multiplicities cancel out.
-    Additionally, `additional` requirements can be encoded for even-indexed
-    interactions and their subsequent odd-indexed interactions.
-    The first and last interactions are "loose" in this encoding and we assume
-    that an external chip takes care of those (as is the case for OpenVM).
-
-    * Assumes that the interactions are already well-ordered.
-    * Assumes that all interactions have the same number of arguments.
-    * Auxiliary variables are named using the prefix `id`.
+    Encodes a permutation check for the given list of interactions. We assume
+    that the interactions are already well-ordered: two consecutive interactions
+    where the first is even-indexed permute (their data is equivalent and their
+    multiplicities cancel out).
     """
     n = len(interactions)
     if n == 0:
@@ -31,10 +30,7 @@ def ordered_permutation_check(
                 logging.warning(f"encoding for possible permutation not yet supported")
                 logging.warning(f"access 1: {d1}")
                 logging.warning(f"access 2: {d2}")
-            if id % 2 == 0:
-                # correct additional on even->odd pairs
-                yield additional(d1, d2)
-            else:
+            if id % 2 == 1:
                 # correct permutation on odd->even pairs
                 yield And(
                     Equals(wrap_mod(Plus(m1, m2)), Int(0)),
