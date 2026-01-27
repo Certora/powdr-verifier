13d720c305e4caf1cc56acd161af498e8eb173a7
Gereon Kremer <gereon@certora.com>
2026-01-27 14:00:44 +0100

    add io relation to encoding

--- src/smt/encoding.py
+++ src/smt/encoding.py
@@ -10,7 +10,24 @@ def collect_variables(data: FormulaWithAxioms) -> frozenset[FNode]:
         *[f.get_free_variables() for f in data.derived],
     )
 
-def build_vc(f1: FormulaWithAxioms, f2: FormulaWithAxioms) -> FNode:
+def build_input_output_relation(name: str, a: dict, b: dict) -> FNode:
+    keys = a.keys() & b.keys()
+    return And(
+        *[
+            with_comment(
+                And(Equals(x, y) for x in a[k] for y in b[k]),
+                f"{name} for {k}"
+            )
+            for k in keys
+        ],
+    )
+
+def build_vc(f1: FormulaWithAxioms, c1: SmtConverter, f2: FormulaWithAxioms, c2: SmtConverter) -> FNode:
+
+    inputs1 = c1.bus_interaction_encoder.get_inputs()
+    inputs2 = c2.bus_interaction_encoder.get_inputs()
+    outputs1 = c1.bus_interaction_encoder.get_outputs()
+    outputs2 = c2.bus_interaction_encoder.get_outputs()
 
     var1 = collect_variables(f1)
     var2 = collect_variables(f2)
@@ -29,6 +46,8 @@ def build_vc(f1: FormulaWithAxioms, f2: FormulaWithAxioms) -> FNode:
             And(*f2.axioms),
             And(*f1.derived),
             And(*f2.derived),
+            build_input_output_relation("INPUT RELATION", inputs1, inputs2),
+            build_input_output_relation("OUTPUT RELATION", outputs1, outputs2),
         )
     )
     return rewrite(f)
