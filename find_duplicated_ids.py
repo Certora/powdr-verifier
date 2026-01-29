e6d8733edab3b20bd4bf3622ef8e26ad6beaf880
Gereon Kremer <gereon@certora.com>
2026-01-29 09:01:29 +0100

    add a little script

---
+++ find_duplicated_ids.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+# Look through the trace-*.smt2 files and look for variables with the same ids.
+# I saw them at some point, but it might have been due to mismatched programs.
+# This script acts as a sanity check to look for cases where powdr would reuse
+# variable ids. It seems it doesn't, but just in case...
+
+import argparse
+from pathlib import Path
+import re
+
+parser = argparse.ArgumentParser()
+parser.add_argument('basedir', type=Path)
+args = parser.parse_args()
+
+r = re.compile(f"\\(declare-fun (.*)@([0-9]+) ")
+
+for test in args.basedir.glob("*"):
+    print(f'looking for duplicates in {test}...')
+    matches = {}
+    for file in test.glob("trace*.smt2"):
+        with open(file, 'r') as f:
+            for m in r.finditer(f.read()):
+                name,id = m.groups()
+                if matches.setdefault(id, name) != name:
+                    print(f"found collision for @{id}: {matches[id]} vs {name}")
