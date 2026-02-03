93cbafaff96e54e70afe06c9f97e076dc62cdc11
Gereon Kremer <gereon@certora.com>
2026-02-03 11:44:56 +0100

    only rewrite equalities

--- src/rewriter/__init__.py
+++ src/rewriter/__init__.py
@@ -24,11 +24,11 @@ class RelationRewriter(substituter.Substituter):
     def __init__(self, env=None):
         substituter.Substituter.__init__(self, env=env)
     
-    @substituter.handles(set(operators.ALL_TYPES) - operators.RELATIONS)
+    @substituter.handles(set(operators.ALL_TYPES) - frozenset([operators.EQUALS]))
     def walk_identity(self, formula, args, **kwargs):
         return keep_comment(substituter.Substituter.super(self, formula, args=args, **kwargs), formula)
 
-    @substituter.handles(operators.RELATIONS)
+    @substituter.handles(frozenset([operators.EQUALS]))
     def walk_identity_or_replace(self, formula, args, **kwargs):
         try:
             res = to_smt(rewrite_one(to_sympy(formula)))
