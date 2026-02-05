5c701adbb67c5676850d5dc7c95eeda8fac054a9
Gereon Kremer <gereon@certora.com>
2026-02-05 12:48:29 +0100

    fix evaluator

--- src/evaluator.py
+++ src/evaluator.py
@@ -49,5 +49,5 @@ def evaluate(input: dict, model: dict[str, int]):
             print("bus interactions are satisfied")
         if eval_list(smt.axioms):
             print("axioms are satisfied")
-        if eval_list(smt.derived):
+        if eval_list([Equals(v, expr) for v, expr in smt.derived]):
             print("derived are satisfied")
