21b6a1e4b592250d739e2b3dd5dc0cd90177208e
Gereon Kremer <gereon@certora.com>
2026-02-05 09:34:21 +0100

    refactor verifier, make completeness work (maybe)

--- src/verifier.py
+++ src/verifier.py
@@ -1,16 +1,52 @@
 import json
 
+# TODO: verify determinism for a couple of examples
+
 
 from .utils.basic_block import BasicBlock
 from .smt.encoding import build_input_output_relation, collect_variables
-from .smt.conversion import SmtConverter, check_formula
+from .smt.conversion import FormulaWithAxioms, SmtConverter, check_formula
 from .smt.utils import *
+
+BEFORE_PREFIX = "before"
+AFTER_PREFIX = "after"
+
+def strip_prefix(name: str) -> str:
+    if name.startswith(BEFORE_PREFIX):
+        return name[len(BEFORE_PREFIX)+1:]
+    elif name.startswith(AFTER_PREFIX):
+        return name[len(AFTER_PREFIX)+1:]
+    return name
+
+def build_model_map(old: frozenset[FNode], new: frozenset[FNode], oldf: FormulaWithAxioms, newf: FormulaWithAxioms) -> dict:
+    omap = { strip_prefix(v.symbol_name()): v for v in old }
+    nmap = { strip_prefix(v.symbol_name()): v for v in new }
+    ndmap = { strip_prefix(v[0].symbol_name()): v for v in newf.derived }
+
+    res = {
+        k: Equals(nmap[k], omap[k]) for k in (omap.keys() & nmap.keys())
+    } | {
+        k: Equals(v[0], v[1]) for (k,v) in ndmap.items()
+    }
+    assert nmap.keys() == res.keys(), "model map for is not complete"
+    return res
+
+def do_check(f: FNode, name: str):
+    match check_formula(f, name):
+        case False,_:
+            print(f"{name} is proven")
+        case None,_:
+            print(f"could not solve {name}, solver returned UNKNOWN")
+        case True,model:
+            print(f"{name} is violated")
+            model = to_nice_model(model)
+            print(json.dumps(model, indent=4))
 
 def verify(before: FNode, after: FNode, block: BasicBlock):
 
     with (
-        SmtConverter("before", block) as before_conv,
-        SmtConverter("after", block) as after_conv
+        SmtConverter(BEFORE_PREFIX, block) as before_conv,
+        SmtConverter(AFTER_PREFIX, block) as after_conv
     ):
         before_smt = before_conv.to_formula_with_axioms(before)
         after_smt = after_conv.to_formula_with_axioms(after)
@@ -21,16 +57,61 @@ def verify(before: FNode, after: FNode, block: BasicBlock):
         inputs2 = after_conv.bus_interaction_encoder.get_inputs()
         outputs1 = before_conv.bus_interaction_encoder.get_outputs()
         outputs2 = after_conv.bus_interaction_encoder.get_outputs()
-        iorelation = And(
-            build_input_output_relation("INPUT RELATION", inputs1, inputs2),
-            build_input_output_relation("OUTPUT RELATION", outputs1, outputs2),
-        )
+        input_relation = build_input_output_relation("INPUT RELATION", inputs1, inputs2)
+        output_relation = build_input_output_relation("OUTPUT RELATION", outputs1, outputs2)
 
 
     # obtain variables and globals
     var1 = collect_variables(before_smt)
     var2 = collect_variables(after_smt)
     globals = before_smt.globals | after_smt.globals
+
+    model_map = build_model_map(var1 - globals, var2 - globals, before_smt, after_smt)
+
+    # check completeness
+    completeness = ForAll(var2 - globals,
+        And(
+            Not(
+                Implies(
+                    And(
+                        *before_smt.constraints,
+                        *before_smt.bus_interactions,
+                        with_comment(And(*model_map.values()), "MODEL MAP"),
+                    ),
+                    And(
+                        *after_smt.constraints,
+                        *after_smt.bus_interactions,
+                    )
+                )
+            ),
+            And(*before_smt.axioms),
+            And(*after_smt.axioms),
+        )
+    )
+    do_check(completeness, "completeness")
+    return
+
+    # determinism: if an input has a trace for both programs, the outputs are the same
+    determinism = And(
+        Not(
+            Implies(
+                And(
+                    *before_smt.constraints,
+                    *before_smt.bus_interactions,
+                    *after_smt.constraints,
+                    *after_smt.bus_interactions,
+                    input_relation,
+                ),
+                And(
+                    common_intermediates,
+                    output_relation
+                )
+            )
+        ),
+        And(*before_smt.axioms),
+        And(*after_smt.axioms),
+    )
+    do_check(determinism, "determinism")
 
     # check soundness
     soundness =  ForAll(var1 - globals,
@@ -52,44 +133,4 @@ def verify(before: FNode, after: FNode, block: BasicBlock):
             And(*after_smt.axioms),
         )
     )
-    #soundness = rewrite(soundness)
-    match check_formula(soundness, "sound"):
-        case False,_:
-            print("Soundness is proven")
-        case None,_:
-            print("Could not solve formula, solver returned UNKNOWN")
-        case True,model:
-            print("Soundness is violated")
-            model = to_nice_model(model)
-            print(json.dumps(model, indent=4))
-
-    # check completeness
-    completeness = ForAll(var2 - globals,
-        And(
-            Not(
-                Implies(
-                    And(
-                        *before_smt.constraints,
-                        *before_smt.bus_interactions,
-                        iorelation,
-                    ),
-                    And(
-                        *after_smt.constraints,
-                        *after_smt.bus_interactions,
-                    )
-                )
-            ),
-            And(*before_smt.axioms),
-            And(*after_smt.axioms),
-        )
-    )
-    #completeness = rewrite(completeness)
-    match check_formula(completeness, "complete"):
-        case False,_:
-            print("Completeness is proven")
-        case None,_:
-            print("Could not solve formula, solver returned UNKNOWN")
-        case True,model:
-            print("Completeness is violated")
-            model = to_nice_model(model)
-            print(json.dumps(model, indent=4))
+    do_check(soundness, "soundness")
