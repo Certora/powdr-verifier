c51413e50027e7fc107241fd214d34691c9b0bbd
Gereon Kremer <gereon@certora.com>
2026-02-05 12:07:54 +0100

    work on range checker: fix issue for non-variable checks

--- src/bus_interactions/openvm_variable_range_checker.py
+++ src/bus_interactions/openvm_variable_range_checker.py
@@ -14,6 +14,12 @@ class OpenVMVariableRangeCheckerEncoder(SingleInteractionEncoder):
         curbits = 25
         if bits.is_int_constant() and bits.constant_value() <= 25:
             curbits = bits.constant_value()
+        
+        if not x.is_symbol():
+            x = wrap_mod(x)
+        
+        if mult.is_int_constant() and mult.constant_value() != 0:
+            return And(LE(Int(0), x), LT(x, Int(2 ** curbits)))
 
         return Implies(
             Not(Equals(mult, Int(0))),
