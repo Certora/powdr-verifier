afb5221288edefb4a9ed7345a13ac3870bce1bc0
Gereon Kremer <gereon@certora.com>
2026-02-09 11:03:57 +0100

    work on rewriter

--- src/rewriter/rewrites.py
+++ src/rewriter/rewrites.py
@@ -1,16 +1,46 @@
-from pysmt.fnode import FNode
+import logging
 from ..utils.args import ARGS
-
 from ..utils.profiling import simple_profile
-from .utils import unpack_modeq
+from ..smt.utils import *
 
+@simple_profile
+def rewrite_simplify(node_type: int, args: list[FNode]) -> FNode:
+    node = get_env().formula_manager.create_node(node_type, tuple(args))
+    res = simplify(node)
+    if res != node:
+        return res
+    return None
 
-def rewrite_mod(node: FNode) -> FNode:
-    if not node.is_mod():
-        return None
-    expr, modulus = node.args()
-    if not expr.is_symbol():
-        return None
+@simple_profile
+def rewrite_mod(node_type: int, args: list[FNode]) -> FNode:
+    assert node_type == operators.MOD
+    expr, modulus = args
+    if expr.is_int_constant():
+        return Int(expr.constant_value() % modulus.constant_value())
     if not modulus.is_int_constant() or modulus.constant_value() != ARGS().field_type.value:
         return None
-    return expr
+    if expr.is_symbol():
+        return expr
+    return None
+
+@simple_profile
+def rewrite_eqmod(node_type: int, args: list[FNode]) -> FNode:
+    assert node_type == operators.EQUALS
+    lhs, rhs = args
+    if not lhs.is_mod() or not rhs.is_zero():
+        return None
+    expr, modulus = lhs.args()
+    if not modulus.is_int_constant() or modulus.constant_value() != ARGS().field_type.value:
+        return None
+    if expr.is_plus() and len(expr.args()) == 2:
+        a, b = expr.args()
+        if a.is_int_constant() and b.is_symbol():
+            return Equals(b, wrap_mod(Int(-a.constant_value())))
+        if a.is_symbol() and b.is_int_constant():
+            return Equals(a, wrap_mod(Int(-b.constant_value())))
+    if expr.is_minus() and len(expr.args()) == 2:
+        a, b = expr.args()
+        if a.is_int_constant() and b.is_symbol():
+            return Equals(b, wrap_mod(Int(a.constant_value())))
+        if a.is_symbol() and b.is_int_constant():
+            return Equals(a, wrap_mod(Int(b.constant_value())))
