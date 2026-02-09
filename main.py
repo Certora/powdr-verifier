829b0dc75d1e0b8d839605a19073883a55bab545
Gereon Kremer <gereon@certora.com>
2026-02-09 11:02:46 +0100

    better error handling

--- main.py
+++ main.py
@@ -16,42 +16,43 @@ if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO, force=True)
     parse_args()
 
-    match ARGS().command:
-        case 'trace':
-            logging.info(f"running tracer on {ARGS().input}")
-            input = load_apc_dump(ARGS().input, 'input')
+    try:
+        match ARGS().command:
+            case 'trace':
+                logging.info(f"running tracer on {ARGS().input}")
+                input = load_apc_dump(ARGS().input, 'input')
 
-            trace(input)
+                trace(input)
 
-        case 'eval':
-            logging.info(f"evaluating trace from {ARGS().model} on {ARGS().input}")
-            input = load_apc_dump(ARGS().input, 'input')
-            model = load_json(ARGS().model, 'model')
+            case 'eval':
+                logging.info(f"evaluating trace from {ARGS().model} on {ARGS().input}")
+                input = load_apc_dump(ARGS().input, 'input')
+                model = load_json(ARGS().model, 'model')
 
-            evaluate(input, model)
+                evaluate(input, model)
 
-        case 'diff':
-            logging.info(f"diffing {ARGS().input_before} and {ARGS().input_after}")
-            diff()
+            case 'diff':
+                logging.info(f"diffing {ARGS().input_before} and {ARGS().input_after}")
+                diff()
 
-        case 'text':
-            logging.info(f"converting {ARGS().input} to text")
-            input = load_apc_dump(ARGS().input, 'input')
-            s = StringIO()
-            converter.text(s, input)
-            print(s.getvalue())
+            case 'text':
+                logging.info(f"converting {ARGS().input} to text")
+                input = load_apc_dump(ARGS().input, 'input')
+                s = StringIO()
+                converter.text(s, input)
+                print(s.getvalue())
 
-        case 'verify':
-            logging.info(f"verify equivalence of {ARGS().input_before} and {ARGS().input_after}")
-            before = load_apc_dump(ARGS().input_before, 'before')
-            after = load_apc_dump(ARGS().input_after, 'after')
+            case 'verify':
+                logging.info(f"verify equivalence of {ARGS().input_before} and {ARGS().input_after}")
+                before = load_apc_dump(ARGS().input_before, 'before')
+                after = load_apc_dump(ARGS().input_after, 'after')
 
-            before_block = BasicBlock(before["block"])
-            assert before_block == BasicBlock(after["block"]), "The basic block has changed"
+                before_block = BasicBlock(before["block"])
+                assert before_block == BasicBlock(after["block"]), "The basic block has changed"
 
-            verify(before, after, before_block)
-        case _:
-            logging.error(f"unknown command: {ARGS().command}")
-            exit(1)
-
-    print_profile()
+                verify(before, after, before_block)
+            case _:
+                logging.error(f"unknown command: {ARGS().command}")
+                exit(1)
+    except KeyboardInterrupt:
+        print_profile()
--- orchestrate.py
+++ orchestrate.py
@@ -142,3 +142,5 @@ if __name__ == '__main__':
     
     except subprocess.CalledProcessError:
         pass
+    except KeyboardInterrupt:
+        pass
