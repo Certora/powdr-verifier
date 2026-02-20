266b57748ce5c3f82cf3aa19e2825c9724f65247
Gereon Kremer <gereon.kremer@gmail.com>
2026-02-20 12:55:30 +0100

    also pass --profile on

--- orchestrate.py
+++ orchestrate.py
@@ -32,10 +32,13 @@ def parse_args():
     parser.add_argument('--clean', action='store_true')
     parser.add_argument("-v", "--verbose", action="count", default=0)
     parser.add_argument("--with-patch", type=Path, default=None)
+    parser.add_argument("--profile", action="store_true")
 
     global _ARGS
     _ARGS = parser.parse_args()
-    _ARGS.verbose_args = ["-v"] * _ARGS.verbose
+    _ARGS.additional_args = []
+    _ARGS.additional_args += ["-v"] * _ARGS.verbose
+    _ARGS.additional_args += ["--log-profile"] if _ARGS.profile else []
     return _ARGS
 
 
@@ -65,7 +68,7 @@ def run_trace(first, *files):
     for f in files:
         subprocess.run([
             "python3", VERIFIER_DIR / "main.py",
-            *_ARGS.verbose_args,
+            *_ARGS.additional_args,
             "--dump-smt",
             "--base-dump", first,
             "trace",
@@ -94,21 +97,19 @@ def run_eval(first, *files):
             continue
         subprocess.run([
             "python3", VERIFIER_DIR / "main.py",
-            *_ARGS.verbose_args,
+            *_ARGS.additional_args,
             "--base-dump", first,
             "eval",
             f,
             model,
         ], check=True)
 
-def run_verify(first, eliminations, pairs):
+def run_verify(first, pairs):
     for a,b in pairs:
         subprocess.run([
             "python3", VERIFIER_DIR / "main.py",
-            *_ARGS.verbose_args,
+            *_ARGS.additional_args,
             "--dump-smt",
-            "--log-profile",
-            *(["--eliminations", eliminations] if eliminations else []),
             "--base-dump", first,
             "verify",
             a,
@@ -129,8 +130,8 @@ if __name__ == '__main__':
     files = sorted((DATA_DIR / args.test).glob("apc_candidate_0_[0-9]*.json"))
     eliminations = DATA_DIR / args.test / "apc_candidate_0_substitutions.json"
 
-    if not eliminations.exists():
-        eliminations = None
+    if eliminations.exists():
+        args.additional_args += ["--eliminations", eliminations]
 
     if not files:
         logging.warning(f"no files found for {args.test}, did you run powdr?")
@@ -158,13 +159,13 @@ if __name__ == '__main__':
             case 'eval-last': run_eval(files[0], files[-1])
             case 'eval-all': run_eval(files[0], *files)
 
-            case 'verify-end2end': run_verify(files[0], eliminations, [(files[0], files[-1])])
-            case 'verify-stepwise': run_verify(files[0], eliminations, itertools.pairwise(files))
-            case 'verify-first': run_verify(files[0], eliminations, [files[:2]])
+            case 'verify-end2end': run_verify(files[0], [(files[0], files[-1])])
+            case 'verify-stepwise': run_verify(files[0], itertools.pairwise(files))
+            case 'verify-first': run_verify(files[0], [files[:2]])
             case 'verify-k':
                 assert all([k in range(len(files)-1) for k in args.k]), f"all k must be in range 0..{len(files)-2}"
-                run_verify(files[0], eliminations, [(files[k], files[k+1]) for k in args.k])
-            case 'verify-last': run_verify(files[0], eliminations, [files[-2:]])
+                run_verify(files[0], [(files[k], files[k+1]) for k in args.k])
+            case 'verify-last': run_verify(files[0], [files[-2:]])
 
             case _:
                 logging.error(f"unknown command: {args.command}")
