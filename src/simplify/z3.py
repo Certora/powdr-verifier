9c004c9bdaa1867e2683b909aca086b044e1828f
Gereon Kremer <gereon@certora.com>
2026-03-05 10:11:20 +0100

    improve z3 simplify

--- src/simplify/z3.py
+++ src/simplify/z3.py
@@ -1,14 +1,34 @@
+from io import StringIO
+from pysmt.smtlib import script
 import z3
 
-from .utils import convert_script_to_string
+from .utils import _string_to_script
 
-@convert_script_to_string
-def simplify_z3(smt: str) -> str:
+def simplify_z3(smt_script: script.SmtLibScript) -> script.SmtLibScript:
+    prefix = []
+    main = []
+    suffix = []
+    before = True
+    for cmd in smt_script:
+        s = cmd.serialize_to_string(daggify=False)
+        match cmd.name:
+            case "assert" | "declare-fun":
+                before = False
+                main.append(s)
+            case _:
+                if before:
+                    prefix.append(s)
+                else:
+                    suffix.append(s)
+    
     tactics = ("simplify", "propagate-values", "simplify")
-    query = z3.parse_smt2_string(smt)
+    query = z3.parse_smt2_string("\n".join(main))
     goal = z3.Goal()
     goal.add(query)
     simplified = z3.Then(*tactics)(goal).as_expr()
     solver = z3.Solver()
     solver.add(simplified)
-    return solver.sexpr()
+    res = solver.sexpr()
+
+    return _string_to_script("\n".join(prefix + [res] + suffix))
+
