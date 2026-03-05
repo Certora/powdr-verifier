b4a2dd0e65b2773ce64b4563698a483c33ebb37f
Gereon Kremer <gereon.kremer@gmail.com>
2026-03-05 10:10:58 +0100

    make checker actually execute the script, not extract the formula

--- src/checker.py
+++ src/checker.py
@@ -10,31 +10,33 @@ def check():
     parser = SmtLibParser()
     logging.info(f"loading from {ARGS().input}")
     smt_script = parser.get_script_fname(str(ARGS().input))
-    f = smt_script.get_last_formula()
 
     logging.info(f"checking formula with {ARGS().solver}")
     with Solver(
         logic=AUFNIA, name=ARGS().solver, solver_options={":timeout": 60000}
     ) as s:
+        s.set_logic = lambda l: None
         try:
-            s.add_assertion(f)
-            match s.solve():
-                case True:
-                    logging.warning("SAT")
-                    model = to_nice_model(s.get_model())
-                    if ARGS().print_model:
-                        print(json.dumps(model, indent=4))
-                    if ARGS().dump_model:
-                        logging.info(f"dumping model to {ARGS().dump_model}")
-                        with open(ARGS().dump_model, "w") as f:
-                            json.dump(model, f, indent=4)
-                    return True, model
-                case False:
-                    logging.warning("UNSAT")
-                    return False, None
-                case _:
-                    logging.warning("UNKNOWN")
-                    return None, None
+            for cmd in smt_script:
+                res = script.evaluate_command(cmd, s)
+                if cmd.name == "check-sat":
+                    match res:
+                        case True:
+                            logging.warning("SAT")
+                            model = to_nice_model(s.get_model())
+                            if ARGS().print_model:
+                                print(json.dumps(model, indent=4))
+                            if ARGS().dump_model:
+                                logging.info(f"dumping model to {ARGS().dump_model}")
+                                with open(ARGS().dump_model, "w") as f:
+                                    json.dump(model, f, indent=4)
+                            return True, model
+                        case False:
+                            logging.warning("UNSAT")
+                            return False, None
+                        case _:
+                            logging.warning("UNKNOWN")
+                            return None, None
         except SolverReturnedUnknownResultError:
             logging.warning("UNKNOWN")
             return None, None
