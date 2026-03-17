38eabf04dfd4b6eba421cf98fab881896c5d6d98
Gereon Kremer <gereon@certora.com>
2026-03-17 16:05:45 +0100

    add utility

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,6 +1,16 @@
 from typing import Any, Callable, Iterable
 
 
+def s2range(s: str) -> range:
+    match s.split(":", maxsplit=1):
+        case [a, b]:
+            return range(0 if a == "" else int(a), 2**32 if b == "" else int(b))
+        case [index]:
+            return range(int(index), int(index)+1)
+        case _:
+            raise ValueError(f"invalid slice: {s}")
+
+
 def merge_dicts(obj: Iterable, get_key: Callable[[Any], dict]) -> dict:
     """Merge dictionaries produced by `get_key` for each element in `obj` (later keys win)."""
     res = {}
