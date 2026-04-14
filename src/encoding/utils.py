2eacfa510d840e41323a757a01343dfbe09b4252
Gereon Kremer <gereon@certora.com>
2026-04-14 10:59:42 +0200

    move is_valid around

--- src/encoding/utils.py
+++ src/encoding/utils.py
@@ -7,3 +7,13 @@ def as_script(status: str):
             return convert_to_smt_script(formula, status)
         return inner
     return wrapped
+
+def get_is_valid(vars: frozenset[FNode], prefix: str) -> FNode | None:
+    match [v for v in vars if v.symbol_name().startswith(f"{prefix}-is_valid@")]:
+        case []:
+            return None
+        case [is_valid]:
+            return is_valid
+        case _:
+            logging.warning("multiple is_valid variables found, this is not supported")
+            return None
