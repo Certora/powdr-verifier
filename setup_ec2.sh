f0fe901fcd9f21bd727dea759a3dbdaf92f2a70d
Gereon Kremer <gereon.kremer@gmail.com>
2026-04-15 11:01:50 +0200

    document setup on an ec2 instance

---
+++ setup_ec2.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# git clone git@github.com:Certora/powdr-verifier.git verifier/
+
+git clone git@github.com:Certora/powdr.git
+
+sudo apt install python3-venv
+source .venv/bin/activate
+pip install -r verifier/requirements.txt
+pysmt-install --z3 --confirm-agreement
+
+mkdir -p ~/bin/
+python3 verifier/download_z3.py z3-4.15.4 ~/bin/
+chmod +x ~/bin/*
+
+sudo apt install build-essential
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+. "$HOME/.cargo/env"
+
+python3 verifier/orchestrate.py powdr-guest guest-keccak
