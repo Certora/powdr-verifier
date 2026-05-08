cdd0fe3d6b9f310185b004a0f23de1893253268a
Gereon Kremer <gereon@certora.com>
2026-05-08 12:15:29 +0200

    update verifier to use the new skolem mechanism

--- src/utils/args.py
+++ src/utils/args.py
@@ -42,7 +42,6 @@ def __build_parser(skip_subparsers=False):
     parser.add_argument("--eliminations", type=Path, default=None)
     parser.add_argument("--solver", type=str, default="z3-latest")
     parser.add_argument("--skip-rewriting", action="store_true")
-    parser.add_argument("--elim-with-skolem", action="store_true")
     parser.add_argument("--elim-with-model", type=Path, default=None)
     parser.add_argument("--cprofile", action="store_true")
     parser.add_argument("--no-typecheck", action="store_true")
