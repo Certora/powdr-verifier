f73828fa2a1d60254851d60cef0626201851757b
Gereon Kremer <gereon.kremer@gmail.com>
2026-05-12 15:25:24 +0200

    refactor verifier

---
+++ src/verify/__init__.py
@@ -0,0 +1,20 @@
+from dataclasses import dataclass, field
+
+
+@dataclass
+class SetInfo:
+    """Combined set-info commands and extra declarations for an SMT script."""
+    cmds: list = field(default_factory=list)
+    decls: list = field(default_factory=list)
+
+    def __iadd__(self, other: "SetInfo") -> "SetInfo":
+        self.cmds.extend(other.cmds)
+        self.decls.extend(other.decls)
+        return self
+
+
+def combine_setinfo(*parts: SetInfo) -> SetInfo:
+    result = SetInfo()
+    for p in parts:
+        result += p
+    return result
