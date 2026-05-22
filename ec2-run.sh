c9fe5032b64265e0b47db02648382396ba8e285b
Gereon Kremer <gereon.kremer@gmail.com>
2026-05-22 19:17:33 +0200

    fix runner script

--- ec2-run.sh
+++ ec2-run.sh
@@ -20,22 +20,26 @@ source .venv/bin/activate
 
 case "$scenario" in
     keccak)
-        rm -rf "$script_dir/powdr-dumps/guest-keccak/" "$script_dir/reports/guest-keccak/"
+        rm -rf "$script_dir/powdr-dumps/guest-keccak/"
+        rm -rf "$script_dir/data/guest-keccak/"
+        rm -rf "$script_dir/reports/guest-keccak/"
         python3 "$script_dir/orchestrate.py" powdr-guest guest-keccak
         python3 "$script_dir/orchestrate.py" -j28 verify guest-keccak : :
         ;;
     pairing)
         rm -rf \
             "$script_dir/powdr-dumps/guest-pairing/" \
+            "$script_dir/data/guest-pairing/" \
             "$script_dir/reports/guest-pairing/" \
             "$script_dir/powdr-dumps/guest-pairing-selection/" \
+            "$script_dir/data/guest-pairing-selection/" \
             "$script_dir/reports/guest-pairing-selection/"
         python3 "$script_dir/orchestrate.py" powdr-guest guest-pairing
         python3 "$script_dir/select_blocks.py" "$script_dir/powdr-dumps/guest-pairing"
         python3 "$script_dir/orchestrate.py" -j28 verify guest-pairing-selection : :
         ;;
     reth)
-        find "$script_dir/data/reth-selection/" -name '*.smt2' -delete
+        rm -rf "$script_dir/data/reth-selection/"
         rm -rf "$script_dir/reports/reth-selection/"
         python3 "$script_dir/orchestrate.py" -j28 verify reth-selection : :
         ;;
