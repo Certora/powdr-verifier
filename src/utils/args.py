f73dc9bd0ad133e6af132c7812832642997dc744
Gereon Kremer <gereon@certora.com>
2026-05-27 20:40:31 +0200

    use nightly now

--- src/utils/args.py
+++ src/utils/args.py
@@ -41,7 +41,7 @@ def __build_parser(skip_subparsers=False):
     parser.add_argument("--check", action="store_true")
     parser.add_argument("--base-dump", type=Path, default=None)
     parser.add_argument("--eliminations", type=Path, default=None)
-    parser.add_argument("--solver", type=str, default="z3-latest")
+    parser.add_argument("--solver", type=str, default="z3-nightly")
     parser.add_argument("--skip-rewriting", action="store_true")
     parser.add_argument("--elim-with-model", type=Path, default=None)
     parser.add_argument("--cprofile", action="store_true")
