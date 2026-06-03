97c13374fd6e4d4d79e498c45e6dafd5697b44e5
Gereon Kremer <gereon@certora.com>
2026-06-03 11:43:45 +0200

    also run traces

--- ec2-run.sh
+++ ec2-run.sh
@@ -11,6 +11,13 @@ scenario="$1"
 shift
 script_dir="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"
 
+list_trace_block_ids_every_fifth() {
+    ls $script_dir/powdr-dumps/$1/apc_candidate_*.json \
+    | sed -n 's/.*apc_candidate_\([0-9]*\)_.*/\1/p' \
+    | sort -nu \
+    | awk 'NR % 5 == 1'
+}
+
 cd ~/
 
 pushd verifier
@@ -26,6 +33,9 @@ case "$scenario" in
         rm -rf "$script_dir/reports/guest-keccak/"
         python3 "$script_dir/orchestrate.py" powdr-guest guest-keccak
         python3 "$script_dir/orchestrate.py" -j28 verify guest-keccak : : "$@"
+        for bid in $(list_trace_block_ids_every_fifth guest-keccak); do
+            python3 "$script_dir/orchestrate.py" -j28 trace guest-keccak ${bid} : "$@"
+        done
         ;;
     keccak-selection)
         rm -rf \
@@ -40,6 +50,9 @@ case "$scenario" in
             --block-ids "2099556,2099672,2101000,2104104,2104636,2104744,2105036,2106172,2106456,2106476" \
             "$script_dir/powdr-dumps/guest-keccak"
         python3 "$script_dir/orchestrate.py" -j28 verify guest-keccak-selection : : "$@"
+        for bid in $(list_trace_block_ids_every_fifth guest-keccak-selection); do
+            python3 "$script_dir/orchestrate.py" -j28 trace guest-keccak-selection ${bid} : "$@"
+        done
         ;;
     pairing)
         rm -rf \
@@ -52,11 +65,17 @@ case "$scenario" in
         python3 "$script_dir/orchestrate.py" powdr-guest guest-pairing
         python3 "$script_dir/select_blocks.py" "$script_dir/powdr-dumps/guest-pairing"
         python3 "$script_dir/orchestrate.py" -j28 verify guest-pairing-selection : : "$@"
+        for bid in $(list_trace_block_ids_every_fifth guest-pairing); do
+            python3 "$script_dir/orchestrate.py" -j28 trace guest-pairing ${bid} : "$@"
+        done
         ;;
     reth)
         rm -rf "$script_dir/data/reth-selection/"
         rm -rf "$script_dir/reports/reth-selection/"
         python3 "$script_dir/orchestrate.py" -j28 verify reth-selection : : "$@"
+        for bid in $(list_trace_block_ids_every_fifth reth-selection); do
+            python3 "$script_dir/orchestrate.py" -j28 trace reth-selection ${bid} : "$@"
+        done
         ;;
     *)
         echo "unknown scenario: $scenario" >&2
